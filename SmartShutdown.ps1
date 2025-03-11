aefa92e9158f4d2a2820dc97978faa3e37b26fbe
MetalSearch <metalsearch@gmail.com>
2025-03-11 21:15:05 +0100

    Add files via upload

--- SmartShutdown.ps1
+++ SmartShutdown.ps1
@@ -1,65 +1,102 @@
 ﻿<#
- Arrêt de l'ordinateur avec décompte
+=========================================================================================
+.SYNOPSIS
+    Arrêt de l'ordinateur avec décompte
+
+.DESCRIPTION
+    Ce script PowerShell permet d'arrêter l'ordinateur avec un décompte de secondes
+    Tant que le décompte est actif, un utilisateur peut cliquer sur "annuler" afin
+    l'extinction de l'ordinateur. Dans le cas contraire, au bout du décompte ou si
+    aucune session n'est ouverte l'ordinateur s'éteindra.
+
+.PARAMETER
+    Aucun paramètre n'est requis pour l'exécution de ce script.
+
+.TODO
+    Verifier le comportement du script sur des sessions vérouillées
+
+.LINK
+        https://github.com/MetalSearch/PowerShell/tree/main
+=========================================================================================
 #>
 
 # Charger l'assembly System.Windows.Forms
 Add-Type -AssemblyName System.Windows.Forms
+
+# Fonction pour vérifier si une session d'utilisateur est ouverte
+function Get-UserSession {
+  $sessions = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty UserName
+  return $sessions
+}
+
+# Fonction pour éteindre l'ordinateur normalement
+function Stop-ComputerNormally {
+  Write-Host "Aucune session ouverte. L'ordinateur va s'éteindre."
+  Stop-Computer -Force
+}
+
+# Fonction pour informer l'utilisateur et éteindre l'ordinateur avec un décompte
+function Stop-ComputerWithCountdown {
+  # Déclarer les variables de script
+  $script:countdown = 30
+  $script:cancelled = $false
+
+  # Créer un formulaire pour afficher le décompte
+  $form = New-Object System.Windows.Forms.Form
+  $form.Text = "Arrêt de l'ordinateur"
+  $form.Size = New-Object System.Drawing.Size(300, 150)
+  $form.StartPosition = "CenterScreen"
+
+  # Créer un label pour afficher le décompte
+  $script:label = New-Object System.Windows.Forms.Label
+  $script:label.AutoSize = $true
+  $script:label.Location = New-Object System.Drawing.Point(50, 20)
+  $script:label.Text = "L'ordinateur va être arrêté dans $script:countdown secondes."
+  $form.Controls.Add($script:label)
+
+  # Créer un bouton pour annuler l'arrêt
+  $button = New-Object System.Windows.Forms.Button
+  $button.Location = New-Object System.Drawing.Point(100, 70)
+  $button.Size = New-Object System.Drawing.Size(75, 23)
+  $button.Text = "Annuler"
+  $button.Add_Click({
+    $script:cancelled = $true
+    $form.Close()
+  })
+  $form.Controls.Add($button)
+
+  # Créer un timer pour le décompte
+  $timer = New-Object System.Windows.Forms.Timer
+  $timer.Interval = 1000 # 1 seconde
+  $timer.Add_Tick({
+    if ($script:countdown -gt 0 -and -not $script:cancelled) {
+      $script:countdown--
+      $script:label.Text = "L'ordinateur va être arrêté dans $script:countdown secondes."
+    } elseif ($script:countdown -eq 0) {
+      $timer.Stop()
+      $form.Close() # Fermer le formulaire avant d'éteindre
+      Stop-Computer -Force
+    }
+  })
+
+  # Démarrer le timer et afficher le formulaire
+  $timer.Start()
+  $form.ShowDialog()
+
+  # Si le bouton Annuler a été cliqué, arrêter le timer
+  if ($script:cancelled) {
+    $timer.Stop()
+    Write-Host "L'arrêt de l'ordinateur a été annulé."
+  }
+}
+
 # Vérifier si une session d'utilisateur est ouverte
-$sessions = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty UserName
+$sessions = Get-UserSession
 
 if (-not $sessions) {
-    # Aucune session ouverte, éteindre l'ordinateur normalement
-    Write-Host "Aucune session ouverte. L'ordinateur va s'éteindre."
-    Stop-Computer -Force
+  # Aucune session ouverte, éteindre l'ordinateur normalement
+  Stop-ComputerNormally
 } else {
-    # Une session est ouverte, informer l'utilisateur
-    $title = "Arrêt de l'ordinateur"
-    $script:countdown = 30 # Déclarer la variable comme variable de script
-    $script:cancelled = $false # Déclarer la variable comme variable de script
-
-    # Créer un formulaire pour afficher le décompte
-    $form = New-Object System.Windows.Forms.Form
-    $form.Text = $title
-    $form.Size = New-Object System.Drawing.Size(300, 150)
-    $form.StartPosition = "CenterScreen"
-
-    $label = New-Object System.Windows.Forms.Label
-    $label.AutoSize = $true
-    $label.Location = New-Object System.Drawing.Point(50, 20)
-    $label.Text = "L'ordinateur va être arrêté dans $script:countdown secondes."
-    $form.Controls.Add($label)
-
-    $button = New-Object System.Windows.Forms.Button
-    $button.Location = New-Object System.Drawing.Point(100, 70)
-    $button.Size = New-Object System.Drawing.Size(75, 23)
-    $button.Text = "Annuler"
-    $button.Add_Click({
-        $script:cancelled = $true # Mettre à jour la variable de script
-        $form.Close()
-    })
-    $form.Controls.Add($button)
-
-    # Créer un timer pour le décompte
-    $timer = New-Object System.Windows.Forms.Timer
-    $timer.Interval = 1000 # 1 seconde
-    $timer.Add_Tick({
-        if ($script:countdown -gt 0 -and -not $script:cancelled) {
-            $script:countdown--
-            $label.Text = "L'ordinateur va être arrêté dans $script:countdown secondes."
-        } elseif ($script:countdown -eq 0) {
-            $timer.Stop()
-            $form.Close() # Fermer le formulaire avant d'éteindre
-           Stop-Computer -Force
-        }
-    })
-
-    # Démarrer le timer et afficher le formulaire
-    $timer.Start()
-    $form.ShowDialog()
-
-    # Si le bouton Annuler a été cliqué, arrêter le timer
-    if ($script:cancelled) {
-        $timer.Stop()
-        Write-Host "L'arrêt de l'ordinateur a été annulé."
-    }
+  # Une session est ouverte, informer l'utilisateur et éteindre l'ordinateur avec un décompte
+  Stop-ComputerWithCountdown
 }
--- SmartShutdown.readme.md
+++ SmartShutdown.readme.md
@@ -1,8 +1,8 @@
 # SmartShutdown
 
-Ce script fournit une solution pratique pour contrôler l'extinction de l'ordinateur tout en permettant aux utilisateurs connectés d'intervenir si nécessaire.
-
 ## Descriptif
+
+Ce script fournit une solution pratique pour contrôler l'extinction de l'ordinateur tout en permettant aux utilisateurs connectés d'intervenir si nécessaire.
 
 Ce script PowerShell permet de fermer l'ordinateur en décomptant un temps fixé en secondes, avec la possibilité d'annuler l'arrêt si nécessaire.
 
@@ -17,8 +17,27 @@ Ce script PowerShell permet de fermer l'ordinateur en décomptant un temps fixé
    
    - Si aucun utilisateur n'est connecté, l'ordinateur s'éteint immédiatement.
    - Si un utilisateur est connecté et non verrouillé, le décompte commence.
+   - Si la session est verrouillée (écran de verrouillage), l'arrêt est annulé.
 
 3. **Interface graphique :**
    
    - Ce script utilise des éléments Windows Forms pour créer une interface simple.
 
+4. **Gestion du temps :**
+   
+   - Un timer démarre le compte à rebours toutes les secondes.
+   - Si l'utilisateur clique sur "Annuler", l'arrêt est annulé ou alors exécuté si le compteur arrive à zéro sans interaction de l’utilisateur.
+
+
+
+# 
+
+### Résumé des fonctionnalités :
+
+- **Vérification de connexion** : Si personne n'est connecté, il arrête immédiatement.
+
+- **Interface utilisateur** : Affiche un formulaire avec un compteur et un bouton d'annulation.
+
+- **Démarrage du décompte** : 30 secondes pour permettre aux utilisateurs de se déconnecter ou sauvegarder.
+
+Ce script est utile pour une fermeture contrôlée, évitant la perte de données non-sauvegardées.
